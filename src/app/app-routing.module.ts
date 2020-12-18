618ce3ab33c24a084a7d839529aa7fd390b2c92f
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2020-12-18 21:48:56 +0000

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './comp/about/about.component';
+import { HomeComponent } from './comp/home/home.component';
+import { ProjectComponent } from './comp/project/project.component';
+import { ContactComponent } from './comp/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent,
+  },
+  {
+    path: 'project', component: ProjectComponent,
+  },
+  {
+    path: 'about', component: AboutComponent,
+  },
+  {
+    path: 'contact', component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './comp/about/about.component';
+import { ProjectComponent } from './comp/project/project.component';
+import { HomeComponent } from './comp/home/home.component';
+import { ContactComponent } from './comp/contact/contact.component';
+import { HeaderComponent } from './comp/header/header.component';
+import { FooterComponent } from './comp/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectComponent,
+    HomeComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
